b490d02fa17db5c4c3f9354ba25c1ba05d8267de
Gustavo <gustavo.prog40@gmail.com>
2021-11-24 14:20:51 -0300

    aaaaa

--- src/components/FeatureMovie.js
+++ src/components/FeatureMovie.js
@@ -1,7 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './fetureMovie.css'
 
 function FeatureMovie({ item }) {
+  console.log(item)
+
+  let firstDate = new Date(item.first_air_date)
+  let genres = []
+
+  for (let i in item.genres) {
+    genres.push(item.genres[i].name)
+  }
+
   return (
     <section
       className="featured"
@@ -12,7 +22,26 @@ function FeatureMovie({ item }) {
       }}
     >
       <div className="featured--vertical">
-        <div className="featured--horizontal"></div>
+        <div className="featured--horizontal">
+          <div className="featured-name">{item.original_name}</div>
+          <div className="featured-info">
+            <div className="featured-points">{item.vote_average} pontos</div>
+            <div className="featured-year">{firstDate.getFullYear()}</div>
+            <div className="featured-seasons">
+              {item.number_of_seasons} temporada{item.number_of_seasons !== 1 ? 's' : ' '}
+            </div>
+            <div className="featured-descriptions">{item.overview} </div>
+            <div className="featured-buttons">
+              <a href={`/watch/${item.id}`} className="featured-watch-button">
+                - Assistir
+              </a>
+              <a href={`/list/add/${item.id}`} className="featured-mylist-button">
+                + Minha Lista
+              </a>
+            </div>
+            <div className="featured-genres">{genres.join(', ')}</div>
+          </div>
+        </div>
       </div>
     </section>
   )
